21db8d349bf80a4a788b4ddd5b3c4febe37393ce
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-30 18:56:05 +0200

    Completed task: T06.ListOfNames

---
+++ Arrays and nasted arrays - Exercise/T06.ListOfNames.js
@@ -0,0 +1,9 @@
+function solve(names){
+    names.sort();
+
+    for (let i = 0; i < names.length; i++){
+        console.log(`${i + 1}.${names[i]}`);
+    }
+}
+
+solve(["John", "Bob", "Christina", "Ema"]);
