a93a8bff01d718a002011a53563b0ae67658f62c
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-12-30 18:54:32 +0200

    Completed task: T02.PrintEveryNthElementFromTheArray

---
+++ Arrays and nasted arrays - Exercise/T02.PrintEveryNthElementFromTheArray.js
@@ -0,0 +1,15 @@
+function solve(array, step){
+    let result = [];
+    for (let i = 0; i < array.length; i+=step){
+        result.push(array[i]);
+    }
+
+    return result;
+}
+
+console.log(solve(['1', 
+'2',
+'3', 
+'4', 
+'5'], 
+6));
