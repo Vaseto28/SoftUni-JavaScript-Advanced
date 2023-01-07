a6821a36274e46b407871558abde35fb581da7e5
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-01-08 00:02:20 +0200

    Completed task: T03.ExtensibleObject

---
+++ Prototypes and inheritance - Exercise/T03.ExtensibleObject.js
@@ -0,0 +1,19 @@
+function solve(){
+    class Baloon{
+
+    }
+
+    class PartyBaloon extends Baloon{
+
+    }
+
+    class BirthdayBaloon extends PartyBaloon{
+
+    }
+
+    return {
+        Baloon, 
+        PartyBaloon,
+        BirthdayBaloon
+    }
+}
